4100691700da4516fcd9e3ff722085ece12b8282
rvm200584 <muthuccna@gmail.com>
2025-12-28 11:30:51 +0530

    initial commit

---
+++ src/main/java/com/example/devops_project1/DevopsProject1Application.java
@@ -0,0 +1,21 @@
+package com.example.devops_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DevopsProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsProject1Application.class, args);
+	}
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome TCS DevOps Architect</h1>";
+	}
+
+}
